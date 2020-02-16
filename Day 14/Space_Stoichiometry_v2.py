c777de82735eee40821ba388f82eb4272f5ec6f7
blhelias <brieuc.lhelias@gmail.com>
2020-02-16 03:30:42 +0100

    Day 14

--- Day 14/Space_Stoichiometry_v2.py
+++ Day 14/Space_Stoichiometry_v2.py
@@ -1,3 +1,5 @@
+# the code was copied from https://www.youtube.com/watch?v=soZ0pzeGbvo
+import math
 from typing import List, NamedTuple
 from collections import namedtuple
 
@@ -12,17 +14,42 @@ class Amount(NamedTuple):
 
     @staticmethod
     def from_string(raw: str) -> 'Amount':
-        chemical, qty = raw.strip().split(" ")
-        return Amount(chemical, qty)
+        qty, chemical = raw.strip().split(" ")
+        return Amount(chemical, int(qty))
 
-
-def parse_input(obj: str):
+def parse_rule(obj: str):
     s = obj.split(" => ")
     right = s[1].split(" ")
     left = s[0].split(", ")
     input_amount = [Amount.from_string(l) for l in left]
-    output_amount = Amount(right[1], right[0])
+    output_amount = Amount(right[1], int(right[0]))
     return Rule(input_amount, output_amount)
+
+def least_ore(rules: List[Rule], fuel_q):
+    rules_by_product = {rule.output.chemical: rule for rule in rules}
+    ore_needed = 0
+    rep = 0
+    
+    requirements = {"FUEL": fuel_q}
+
+    def done() -> bool:
+        return  all(qty <= 0 for qty in requirements.values())
+
+    while not done():
+        key = next(iter(chem for chem, qty in requirements.items() if qty > 0))
+        qty_needed = requirements[key]
+
+        rule = rules_by_product[key]
+        num_times = math.ceil(qty_needed / rule.output.quantity)
+        requirements[key] -= num_times * rule.output.quantity
+
+        for amount in rule.input:
+            if amount.chemical == "ORE":
+                ore_needed += amount.quantity * num_times
+            else:
+                requirements[amount.chemical] = requirements.get(amount.chemical, 0) + num_times * amount.quantity
+        
+    return ore_needed
 
 
 if __name__ == "__main__":
@@ -35,6 +62,15 @@ if __name__ == "__main__":
 7 DCFZ, 7 PSHF => 2 XJWVT
 165 ORE => 2 GPVTF
 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"""
-    i = TEST1.split("\n")
-    print(i)
-    p = [parse_input(raw) for raw in i]
+
+    # rules = [parse_rule(raw) for raw in TEST1.split("\n")]
+    # print(least_ore(rules))
+
+    with open("input.txt", 'r') as t:
+        TEST = t.read()
+
+    rules = [parse_rule(raw) for raw in TEST.split("\n")]
+    fuel_q = range(1863000,1864100)
+
+    print(least_ore(rules, 1863741))
+    print(least_ore(rules, 1863742))
