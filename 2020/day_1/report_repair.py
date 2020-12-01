6148e65e25362750c486e48bf902fd208adfc507
blhelias <brieuc.lhelias@gmail.com>
2020-12-01 14:33:41 +0100

    update

--- 2020/day_1/report_repair.py
+++ 2020/day_1/report_repair.py
@@ -7,12 +7,10 @@ def read_input(elements_type=str):
 
 if __name__=="__main__":
     X = read_input(int)
-    with open("input.txt", "r") as f:
-        X = list(map(int, f.read().splitlines()))
-        n = len(list(X))
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    if X[i] + X[j] + X[k] == 2020:
-                        print(X[i] * X[j] * X[k])
-                        break
+    n = len(list(X))
+    for i in range(n):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                if X[i] + X[j] + X[k] == 2020:
+                    print(X[i] * X[j] * X[k])
+                    break
