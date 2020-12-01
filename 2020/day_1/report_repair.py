b648da2a093f7e697f98508d98277b6f67fddf0b
blhelias <brieuc.lhelias@gmail.com>
2020-12-01 13:43:32 +0100

    clean day1

--- 2020/day_1/report_repair.py
+++ 2020/day_1/report_repair.py
@@ -1,12 +1,18 @@
 """Day 1 advent of code"""
-if __name__=="__main__":
-    # Read input file
+
+def read_input(elements_type=str):
     with open("input.txt", "r") as f:
-        exp_report = list(map(int, f.read().splitlines()))
-        n = len(list(exp_report))
+        l = list(map(elements_type, f.read().splitlines()))
+    return l
+
+if __name__=="__main__":
+    X = read_input(int)
+    with open("input.txt", "r") as f:
+        X = list(map(int, f.read().splitlines()))
+        n = len(list(X))
         for i in range(n):
             for j in range(i+1, n):
-                for k in range(i+2, n):
-                    if exp_report[i] + exp_report[j] + exp_report[k] == 2020:
-                        print(exp_report[i] * exp_report[j] * exp_report[k])
+                for k in range(j+1, n):
+                    if X[i] + X[j] + X[k] == 2020:
+                        print(X[i] * X[j] * X[k])
                         break
