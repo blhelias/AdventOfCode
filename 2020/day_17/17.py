eaf8da6d149d9c88db63f89ebd9ce630c3f9ca7e
blhelias <brieuc.lhelias@gmail.com>
2020-12-24 12:15:31 +0100

    part 2 day17

--- 2020/day_17/17.py
+++ 2020/day_17/17.py
@@ -2,50 +2,31 @@
 
 
 class GameOfLife:
-    def __init__(self, grid=None, dimension=2, rounds=6, shape=5):
+    def __init__(self, grid=None, rounds=6, shape=(5, 5, 5)):
         self.grid = grid
         self.rounds = rounds
-        self.shape = shape
+        self.shape = tuple(x if x%2 == 1 else x+1 for x in shape)
 
     def from_string(self, s):
-        if self.shapeension = 
-
-    def from_string_3d(self, s):
         # Add padding to initial 2d grid
         x = s.split("\n")
-        padd_x = (self.shape - len(x)) // 2
-        padd_y = (self.shape - len(x[0])) // 2 
+
+        if len(x) > self.shape[0] or len(x[0]) > self.shape[1]:
+            raise ValueError(
+                "Invalid Shape Input: Shape value must be greater than grid's shape"
+            )
+
+        padd_x = (self.shape[0] - len(x)) // 2
+        padd_y = (self.shape[1] - len(x[0])) // 2 
         x = ["." * padd_y + u + "." * padd_y for u in x]  
-        padd = ["." * self.shape for _ in range(padd_x)]
+        padd = ["." * self.shape[0] for _ in range(padd_x)]
         x = padd + x + padd
-
-        empty_g = ["." * self.shape for r in range(self.shape)]
-        result = []
-
-        for i in range(self.shape): 
-            if i == self.shape // 2:
-                result.append(x)
-            else:
-                result.append(empty_g)
-
-        self.grid = result
-        return result
-
-    def from_string_4d(self, s):
-        # Add padding to initial 2d grid
-        x = s.split("\n")
-        padd_x = (self.shape - len(x)) // 2
-        padd_y = (self.shape - len(x[0])) // 2 
-        x = ["." * padd_y + u + "." * padd_y for u in x]  
-        padd = ["." * self.shape for _ in range(padd_x)]
-        x = padd + x + padd
-
-        empty_g = ["." * self.shape for r in range(self.shape)]
+        empty_g = ["." * self.shape[1] for r in range(self.shape[0])]
         result = [] 
-        for i in range(self.shape): 
+        for i in range(self.shape[3]): 
             temp = []
-            for j in range(self.shape): 
-                if j == self.shape // 2 and i == self.shape // 2:
+            for j in range(self.shape[2]): 
+                if j == self.shape[2] // 2 and i == self.shape[3] // 2:
                     temp.append(x)
                 else:
                     temp.append(empty_g)
@@ -53,30 +34,10 @@ class GameOfLife:
             result.append(temp)
 
         self.grid = result
-        print(result)
-        print(len(result))
-        print(len(result[0]))
-        print(len(result[0][0]))
-        print(len(result[0][0][0]))
         return result
 
-    def run(self, cycle):
-        for c in range(cycle):
-            result = []
-            for i in range(self.shape):
-                temp = []
-                for j in range(self.shape):
-                    s = ""
-                    for k in range(self.shape):
-                        s += self.__apply_rules(i, j, k)
-
-                    temp.append(s)
-                result.append(temp)
-
-            self.grid = result
-
-    def __apply_rules_4d(self, x, y, z, w):
-        cell = self.grid[x][y][z][w]
+    def __apply_rules(self, x, y, z, w):
+        cell = self.grid[w][z][x][y]
         actives = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
@@ -88,16 +49,14 @@ class GameOfLife:
                         z_coor += k
                         w_coor += p
                         if (
-                            0 <= x_coor < len(self.grid) and \
-                            0 <= y_coor < len(self.grid[0]) and \
-                            0 <= z_coor < len(self.grid[0][0]) and \
-                            0 <= w_coor < len(self.grid[0][0][0]) and \
-                            abs(i) + abs(j) + abs(k) + abs(p) != 0
-                        ):
-                            if self.grid[x_coor][y_coor][z_coor][w_coor] == "#":
+                            0 <= x_coor < len(self.grid[0][0]) and \
+                            0 <= y_coor < len(self.grid[0][0][0]) and \
+                            0 <= z_coor < len(self.grid[0]) and \
+                            0 <= w_coor < len(self.grid) and \
+                            abs(i) + abs(j) + abs(k) + abs(p) != 0):
+                            if self.grid[w_coor][z_coor][x_coor][y_coor] == "#":
                                 actives += 1
-
-        if (actives == 2 or actives == 3) and cell == "#":
+        if (actives == 2 or actives == 3) and cell == "#":            
             next_status = "#"
         elif actives == 3 and cell == ".":
             next_status = "#"
@@ -106,17 +65,17 @@ class GameOfLife:
 
         return next_status
 
-    def run_4d(self, cycle):
+    def run(self, cycle):
         for c in range(cycle):
             result = []
-            for i in range(self.shape):
+            for w in range(self.shape[3]):
                 temp1 = []
-                for j in range(self.shape):
+                for z in range(self.shape[2]):
                     temp2 = []
-                    for k in range(self.shape):
+                    for x in range(self.shape[0]):
                         s = ""
-                        for p in range(self.shape):
-                            s += self.__apply_rules_4d(i, j, k, p)
+                        for y in range(self.shape[1]):
+                            s += self.__apply_rules(x, y, z, w)
 
                         temp2.append(s)
                     temp1.append(temp2)
@@ -124,45 +83,7 @@ class GameOfLife:
 
             self.grid = result
 
-    def __apply_rules(self, x, y, z):
-        cell = self.grid[x][y][z]
-        actives = 0
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                for k in range(-1, 2):
-                    x_coor, y_coor, z_coor = x, y, z
-                    x_coor += i
-                    y_coor += j
-                    z_coor += k
-                    if (
-                        0 <= x_coor < len(self.grid) and \
-                        0 <= y_coor < len(self.grid[0]) and \
-                        0 <= z_coor < len(self.grid[0][0]) and \
-                        abs(i) + abs(j) + abs(k) != 0
-                    ):
-                        if self.grid[x_coor][y_coor][z_coor] == "#":
-                            actives += 1
-
-        if (actives == 2 or actives == 3) and cell == "#":
-            next_status = "#"
-        elif actives == 3 and cell == ".":
-            next_status = "#"
-        else:
-            next_status = "."
-
-        return next_status
-
     def get_count_active(self):
-        actives  = 0
-        for g in self.grid:
-            for rows in g:
-                for cube in rows:
-                    if cube == "#":
-                        actives += 1
-
-        return actives 
-
-    def get_count_active_4d(self):
         actives  = 0
         for w in self.grid:
             for g in w:
@@ -175,9 +96,10 @@ class GameOfLife:
 
     def __repr__(self):
         g = []
-        for z in range(len(self.grid)):
-            g.append(f"z={z}")
-            g.append("\n".join(self.grid[z]))
+        for w in range(len(self.grid)):
+            for z in range(len(self.grid[0])):
+                g.append(f"z={z} | w={w}")
+                g.append("\n".join(self.grid[w][z]))
 
         repr_val  = "\n\n".join(g)
 
@@ -188,16 +110,18 @@ if __name__=="__main__":
     RAW = """.#.
 ..#
 ###"""
-    SHAPE = 20
+    SHAPE_P1 = (21, 21, 21, 1)
+    SHAPE_P2 = (21, 21, 21, 21)
     CYCLE = 6
     with open("input.txt", "r") as f:
         X = f.read()
-        # pass
 
-    # game3d = GameOfLife(shape=SHAPE)
-    # game3d.from_string_3d(RAW)
-    game4d = GameOfLife(shape=SHAPE)
-    # game4d.from_string_4d(RAW)
-    game4d.from_string_4d(X)
-    game4d.run_4d(cycle=CYCLE)
-    print(game4d.get_count_active_4d())
+    game_p1 = GameOfLife(shape=SHAPE_P1)
+    game_p1.from_string(X)
+    game_p1.run(cycle=CYCLE)
+    print(game_p1.get_count_active())
+
+    game_p2 = GameOfLife(shape=SHAPE_P2)
+    game_p2.from_string(X)
+    game_p2.run(cycle=CYCLE)
+    print(game_p2.get_count_active())
